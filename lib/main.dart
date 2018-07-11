966cce0ce6802ffd27e257d33c061cb18d6d2e44
hellolad <>
2018-07-11 10:41:44 +0800

    优化代码，数据和展示分离。

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,80 @@
 import 'package:flutter/material.dart';
-import 'Today.dart';
-import 'Discover.dart';
+import 'today/today.dart';
+import 'discover/discover.dart';
+import 'collection/collection.dart';
+import 'profile/profile.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Youyaoqi(),
-    theme: ThemeData(primaryColor: Colors.green),
+    home: MyApp(),
+    theme: ThemeData(
+      buttonColor: Colors.green,
+      backgroundColor: Colors.grey[200],
+      textTheme: TextTheme(
+        display1: TextStyle(fontSize: 17.0, color: Colors.black),
+        display2: TextStyle(fontSize: 13.0, color: Colors.grey),
+        display3: TextStyle(fontSize: 23.0, color: Colors.black)
+      ),
+    ),
   )
 );
 
-
-class Youyaoqi extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
-
-class _MyHomePageState extends State<Youyaoqi> {
+class _MyAppState extends State<MyApp> {
   PageController pageController;
-  int page = 0;
+  int currentIndex = 0;
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    pageController = PageController(initialPage: this.page);
-  }
-  void onTap(int index) {
-    print('index: $index');
-    pageController.animateToPage(index, duration: Duration(microseconds: 1), curve: Curves.ease);
-  }
-  void onPageChanged(int page) {
-    setState(() {
-      print('this.page: ${this.page}');
-      this.page = page;
-    });
+    this.pageController = PageController(initialPage: this.currentIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: PageView(
-        children: [
-          Today(),
-          Discover(),
-          BookCase(),
-          Mine()
-        ],
-        controller: pageController,
-        onPageChanged: onPageChanged,
-      ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.date_range),
-            title: Text('今日'),
+            icon: Icon(Icons.calendar_today),
+            title: Text("今日"),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            title: Text('发现')
+            title: Text("发现"),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            title: Text('书架')
+            icon: Icon(Icons.collections),
+            title: Text("收藏"),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.perm_identity),
-            title: Text('我的')
-          )
+            icon: Icon(Icons.person),
+            title: Text("我的"),
+          ),
         ],
-        fixedColor: Theme.of(context).primaryColor,
+        fixedColor: Colors.green,
         type: BottomNavigationBarType.fixed,
-        onTap: onTap,
-        currentIndex: page,
+        onTap: (page) {
+          pageController.jumpToPage(page);
+        },
+        currentIndex: this.currentIndex,
       ),
-    );
-
-  }
-}
-
-
-class BookCase extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: Center(
-        child: Text('书架'),
-      ),
-    );
-  }
-}
-
-class Mine extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      body: Center(
-        child: Text('我的'),
+      body: PageView(
+        children: <Widget>[
+          TodayWidget(),
+          DiscoverWidget(),
+          CollectionWidget(),
+          ProfileWidget()
+        ],
+        controller: this.pageController,
+        onPageChanged: (index) {
+          setState((){
+            this.currentIndex = index;
+          });
+        },
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:you_yao_qi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new Youyaoqi());
+    await tester.pumpWidget(new MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
